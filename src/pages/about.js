8a2897cc063864aa32dcb488a8f3247805bdded1
ws-h842 <90305450+ws-h842@users.noreply.github.com>
2023-04-05 07:49:21 +0900

    Added Repo URL to About page and Added netlify.toml

--- src/pages/about.js
+++ src/pages/about.js
@@ -28,7 +28,7 @@ const About = ({data}) => {
                     <h1 className="mb-4">About</h1>
                     <p><mark><a href="https://www.thecollegiateacademy.net/" target="_blank">The Collegiate Academy (TCA)</a></mark> is an international school located in Goyang, South Korea. This website is where monthly journal articles of TCA students are uploaded.</p>
                     <p>This website was created and is run by TCA's coding club. We are still updating and adding new features to the website, so we are open to hearing your ideas. If you have any suggestions, questions, report of errors/bugs, etc. regarding the articles or website, please contact through this email: <ins>wonseunghyun248@gmail.com</ins></p>
-                    <p>Source code can be found in <a href="">this repository.</a></p>
+                    <p>Source code can be found in <a href="https://github.com/ws-h842/tca-journal" target="_blank">this repository.</a></p>
                 </section>
             </main>
         </Layout>
