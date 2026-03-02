4899b327fb1708d649725a1cc7cbfebb68e04328
DDIDOLYI <ddidolyi@gmail.com>
2026-03-02 21:31:48 +0900

    Edited About Page

--- src/pages/about.js
+++ src/pages/about.js
@@ -26,7 +26,7 @@ const About = ({data}) => {
                 <section>
                     <div className="flex-column"><GatsbyImage image={tca_logo} /></div>
                     <h1 className="mb-4">About</h1>
-                    <p><mark><a href="https://www.thecollegiateacademy.net/" target="_blank">The Collegiate Academy (TCA)</a></mark> is an international school in Goyang, South Korea. This is where monthly articles authored by TCA students are uploaded. Developed by TCA's coding club, the website is continually adding new features. Please don't hesitate to reach out with any suggestions or bug reports via email at <ins>wonseunghyun248@gmail.com</ins></p>
+                    <p><mark><a href="https://www.thecollegiateacademy.net/" target="_blank">The Collegiate Academy (TCA)</a></mark> is an international school in Goyang, South Korea. This is where monthly articles authored by TCA students are uploaded. Developed by TCA's coding club, the website is continually adding new features. Please don't hesitate to reach out with any suggestions or bug reports via email at <ins>ddidolyi@gmail.com</ins></p>
                     <p>You can access this website's source code <a href="https://github.com/ws-h842/tca-journal" target="_blank">here</a>.</p>
                 </section>
             </main>
